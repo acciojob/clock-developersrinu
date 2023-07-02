22ca7f218aad47b2612effb99fb8eec62b38ef7c
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-07-02 19:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+   document.addEventListener("DOMContentLoaded", function() {
+      function updateDateTime() {
+        const currentDate = new Date();
+        const formattedDateTime = currentDate.toLocaleString();
+        document.getElementById("timer").textContent = formattedDateTime;
+      }
+
+      // Update the date and time immediately
+      updateDateTime();
+
+      // Update the date and time every second
+      setInterval(updateDateTime, 1000);
+    });
